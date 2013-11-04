6d1556866cb29d48c6a4f77d0f054ff4fd188176
Thorvald Sørensen <thorvald.soerensen@gmail.com>
2013-11-04 19:53:52 +0100

    First push
    Fixed many bugs + cleanup

---
+++ Survival-Games-master/src/main/java/org/mcsg/survivalgames/commands/Join.java
@@ -0,0 +1,62 @@
+package org.mcsg.survivalgames.commands;
+import org.mcsg.survivalgames.SurvivalGames;
+
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.mcsg.survivalgames.GameManager;
+import org.mcsg.survivalgames.MessageManager;
+import org.mcsg.survivalgames.SettingsManager;
+import org.mcsg.survivalgames.MessageManager.PrefixType;
+
+
+public class Join implements SubCommand{
+	private MessageManager msgmgr = MessageManager.getInstance();
+	public boolean onCommand(Player player, String[] args) {
+		if(args.length == 1){
+			if(player.hasPermission(permission())){if (SurvivalGames.econOn && SurvivalGames.econPoints.containsKey("join")){
+				if (SurvivalGames.econ.getBalance(player.getName()) <= SurvivalGames.econPoints.get("join") -0.01){
+					msgmgr.sendMessage(MessageManager.PrefixType.WARNING, "You can not afford to join this game!", player);
+					return true;
+				}
+				SurvivalGames.econ.withdrawPlayer(player.getName(), SurvivalGames.econPoints.get("join"));
+				msgmgr.sendMessage(MessageManager.PrefixType.INFO, SurvivalGames.econPoints.get("join") + " have been withdrawn from your funds.", player);
+			}
+			try {
+				int a = Integer.parseInt(args[0]);
+				GameManager.getInstance().addPlayer(player, a);
+			} catch (NumberFormatException e) {
+				MessageManager.getInstance().sendFMessage(PrefixType.ERROR, "error.notanumber", player, "input-" + args[0]);
+				}
+			}
+			else{
+				MessageManager.getInstance().sendFMessage(PrefixType.WARNING, "error.nopermission", player);
+			}
+		}
+		else{
+			if(player.hasPermission("sg.lobby.join")){
+				if(GameManager.getInstance().getPlayerGameId(player)!=-1){
+					MessageManager.getInstance().sendMessage(PrefixType.ERROR, "error.alreadyingame", player);
+					return true;
+				}
+				player.teleport(SettingsManager.getInstance().getLobbySpawn());
+				return true;
+			}
+			else{
+				MessageManager.getInstance().sendFMessage(PrefixType.WARNING, "error.nopermission", player);
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public String help(Player p) {
+		return "/sg join - " + SettingsManager.getInstance().getMessageConfig().getString("messages.help.join", "Join the lobby");
+	}
+
+	@Override
+	public String permission() {
+		return "sg.arena.join";
+	}
+}
+
---
+++ Survival-Games-master/src/main/java/org/mcsg/survivalgames/events/SignClickEvent.java
@@ -0,0 +1,60 @@
+package org.mcsg.survivalgames.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.mcsg.survivalgames.GameManager;
+import org.mcsg.survivalgames.MessageManager;
+import org.mcsg.survivalgames.SurvivalGames;
+
+
+
+public class SignClickEvent implements Listener{
+
+	private MessageManager msgmgr = MessageManager.getInstance();
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void clickHandler(PlayerInteractEvent e){
+
+        if(!(e.getAction()==Action.RIGHT_CLICK_BLOCK || e.getAction()==Action.LEFT_CLICK_BLOCK)) return;
+        
+
+        Block clickedBlock = e.getClickedBlock(); 
+        if(!(clickedBlock.getType()==Material.SIGN || clickedBlock.getType()==Material.SIGN_POST || clickedBlock.getType()==Material.WALL_SIGN)) return;
+        Sign thisSign = (Sign) clickedBlock.getState();
+        //System.out.println("Clicked sign");
+        String[] lines = thisSign.getLines();
+        if(lines.length<3) return;
+        if(lines[0].equalsIgnoreCase(ChatColor.ITALIC + "[HungerGames]")) {
+        	e.setCancelled(true);
+        	if (SurvivalGames.econOn && SurvivalGames.econPoints.containsKey("join")){
+        		if (SurvivalGames.econ.getBalance(e.getPlayer().getName()) <= SurvivalGames.econPoints.get("join") -0.01){
+        			msgmgr.sendMessage(MessageManager.PrefixType.WARNING, "You can not afford to join this game!", e.getPlayer());
+        			return;
+        		}
+        		SurvivalGames.econ.withdrawPlayer(e.getPlayer().getName(), SurvivalGames.econPoints.get("join"));
+        		msgmgr.sendMessage(MessageManager.PrefixType.INFO, SurvivalGames.econPoints.get("join") + " have been withdrawn from your funds.", e.getPlayer());
+        	}
+        	try{
+        		if(lines[2].equalsIgnoreCase("Auto Assign")){
+        			GameManager.getInstance().autoAddPlayer(e.getPlayer());
+        		}
+        		else{
+        			String game = lines[2].replace("Arena ", "");
+        			int gameno  = Integer.parseInt(game);
+        			GameManager.getInstance().addPlayer(e.getPlayer(), gameno);
+        		}
+
+        	}catch(Exception ek){}
+        }
+
+    }
+
+}
+
+
